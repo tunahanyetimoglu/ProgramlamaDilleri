e27e909bcf7f0afcb57ef586e78885ae611d61ef
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-11-06 22:38:35 +0300

    Update StudentTests.cs

--- sch/schTests/StudentTests.cs
+++ sch/schTests/StudentTests.cs
@@ -12,31 +12,21 @@ namespace sch.Tests
     [TestClass()]
     public class StudentTests
     {
-
         [TestMethod()]
         public void pathControllerTest()
         {
-<<<<<<< HEAD
             Assert.AreEqual<Boolean>(true, File.Exists("../../okul.csv"));
             Assert.AreEqual<Boolean>(false, File.Exists("okul.csv"));
-=======
-            Assert.AreEqual<Boolean>(true, Program.pathController("../../okul.csv"));
-            Assert.AreEqual<Boolean>(false, Program.pathController("okul.csv"));
->>>>>>> 360178fa9959b9500e4cd514a96bb53b5aa7c360
-        }
-
-        
+        }        
         [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
         public void uniqueTest()
         {
             Assert.AreEqual<Boolean>(true, Student.csvUniqueName(Student.Parse("../../okul.csv")));
             Assert.AreEqual<Boolean>(false, Student.csvUniqueName(Student.Parse("../../okul_not_unique.csv")));
         }
-
         [TestMethod()]
         public void argumanControllerTest()
         {
-<<<<<<< HEAD
             Assert.AreEqual<Boolean>(true, Student.argumentController("K"));
             Assert.AreEqual<Boolean>(true, Student.argumentController("k"));
             Assert.AreEqual<Boolean>(true, Student.argumentController("E"));
@@ -49,13 +39,6 @@ namespace sch.Tests
         }
         [TestMethod()]
         public void argumanLengthControl()
-=======
-            Assert.AreEqual<Boolean>(true, Student.argumanController("K"));
-            Assert.AreEqual<Boolean>(false, Student.argumanController("q"));
-        }
-        [TestMethod()]
-        public void argumanLenghtControl()
->>>>>>> 360178fa9959b9500e4cd514a96bb53b5aa7c360
         {
             string[] testArray = new string[] { "1", "K" };
             Assert.AreEqual<Boolean>(false, Program.argsLengthControl(testArray));
@@ -63,13 +46,8 @@ namespace sch.Tests
         [TestMethod()]
         public void argumanControllerErrorMessage()
         {
-<<<<<<< HEAD
             Assert.AreEqual<String>("Devre Hatası", Student.argumentControllerErrorMessage("5"));
             Assert.AreEqual<String>("Arguman Hatası", Student.argumentControllerErrorMessage("erkek"));
-=======
-            Assert.AreEqual<String>("Devre Hatası", Student.argumanControllerErrorMessage("5"));
-            Assert.AreEqual<String>("Arguman Hatası", Student.argumanControllerErrorMessage("erkek"));
->>>>>>> 360178fa9959b9500e4cd514a96bb53b5aa7c360
         }
     }
     
