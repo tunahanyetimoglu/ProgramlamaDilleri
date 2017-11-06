f8369e801aed4264bc438aa4bd2c459d304eab09
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-11-06 22:37:23 +0300

    Update Program.cs

--- sch/sch/Program.cs
+++ sch/sch/Program.cs
@@ -7,59 +7,21 @@ namespace sch
     {      
         public static Boolean argsLengthControl(string[] args)
         {
-<<<<<<< HEAD
             if (args.Length > 1)
                 Console.Error.WriteLine("Hatali Arguman sayisi.");         
 
             return (args.Length < 2);
-=======
-            FileStream fs = null;
-            try
-            {
-                fs = new FileStream(@path, FileMode.Open);
-                return true;
-            }
-            catch (Exception e)
-            {
-                Console.Error.WriteLine("Hata: Dosya bulunamadı!" );
-                return false;
-            }
-            finally
-            {
-                if(fs != null)
-                    fs.Close();
-            }
-        }
-        public static Boolean argsLengthControl(string[] args)
-        {
-            if (args.Length > 1)
-            {
-                Console.Error.WriteLine("Hatali Arguman sayisi.");
-                return false;
-            }
-            else
-                return true;
->>>>>>> 360178fa9959b9500e4cd514a96bb53b5aa7c360
         }
         static void Main(string[] args)
         {
 
             if (!argsLengthControl(args))
-<<<<<<< HEAD
                 System.Environment.Exit(1);
 
             const string Path = "../../okul.csv";
 
             if (!File.Exists(Path))
                 System.Environment.Exit(1); 
-=======
-                System.Environment.Exit(-1);
-
-            const string Path = "../../okul.csv";
-
-            if (!pathController(Path))
-                System.Environment.Exit(-1); 
->>>>>>> 360178fa9959b9500e4cd514a96bb53b5aa7c360
 
             var studentOrderedList = Student.Parse(Path);
 
@@ -76,11 +38,7 @@ namespace sch
             }
             else
             {
-<<<<<<< HEAD
                 Student.argumentControllerErrorMessage(args[0]);
-=======
-                Student.argumanControllerErrorMessage(args[0]);
->>>>>>> 360178fa9959b9500e4cd514a96bb53b5aa7c360
             }
         }
     }
